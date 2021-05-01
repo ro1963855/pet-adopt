e4a0959a2834b40673205f7e07e1c54eb77f97f0
Dandy <dandy.chen@buyandship.com.tw>
2021-05-01 08:57:47 +0800

    add pet list and component

---
+++ next.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  images: {
+    domains: ['asms.coa.gov.tw'],
+  },
+}
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,10 @@
 import Head from 'next/head'
+import Pet from '../components/Pet'
 
-function Home({ data }) {
+function Home({ petList }) {
   return (
     <div>
+      { console.log(petList) }
       <Head>
         <title>領養你的寵物</title>
         <meta name="description" content="幫助大家在各地收容所領養到想要的寵物" />
@@ -16,18 +18,27 @@ function Home({ data }) {
             <a className="navbar-brand" href="#">寵物領養</a>
           </nav>
         </header>
+        <section className="container-fluid">
+          <div className="row petList">
+            {petList.map((petInfo) => (
+              <div className="col-6 col-md-4 col-lg-3 mt-4" key={petInfo.AnimalId}>
+                <Pet info={petInfo}></Pet>
+              </div>
+            ))}
+          </div>
+        </section>
       </main>
     </div>
   )
 }
 
 export async function getServerSideProps(context) {
-  // Fetch data from external API
+  // Fetch petList from external API
   const res = await fetch(`https://asms.coa.gov.tw/Asms/api/ViewNowAnimal?pageSize=200&currentPage=1&sortDirection=DESC&sortFields=AcceptDate`)
-  const data = await res.json()
+  const petList = await res.json()
 
-  // Pass data to the page via props
-  return { props: { data } }
+  // Pass petList to the page via props
+  return { props: { petList } }
 }
 
 export default Home
